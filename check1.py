e1f91c1c2a6c23e6707754afb69001411e3bfa72
eanmikloseygh <eanmiklosey@gmail.com>
2026-03-31 01:46:28 -0400

    Delete check1.py

--- check1.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-df = pd.read_csv(r"C:\Users\leosp\Documents\work1 - Sheet1.csv")
-df.head()
-df.info()
-df.dtypes
